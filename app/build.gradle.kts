9ec7402b258cf9675b7401b4802451f6d9ab135d
Scott Figueroa <scottricardo@ucsb.edu>
2025-10-20 16:46:52 -0700

    changed sdk version to upload app via adb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.testlockscreen.presentation"
-        minSdk = 30
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
