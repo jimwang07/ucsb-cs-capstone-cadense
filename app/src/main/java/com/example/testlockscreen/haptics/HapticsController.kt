e97d63230253852d9b272737a0bd4997d75464fe
Vincent <vcheong0202@gmail.com>
2025-11-03 15:34:40 -0800

    remove log from haptics controller

--- app/src/main/java/com/example/testlockscreen/haptics/HapticsController.kt
+++ app/src/main/java/com/example/testlockscreen/haptics/HapticsController.kt
@@ -4,15 +4,12 @@ import android.content.Context
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
-import android.util.Log
 
 class HapticsController(context: Context) {
 
     private val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
     fun vibrate(milliseconds: Long) {
-        // for debugging purposes only
-        Log.d("HapticsController", "Vibrating for $milliseconds ms")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             vibrator.vibrate(VibrationEffect.createOneShot(milliseconds, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
